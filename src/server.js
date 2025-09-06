0e85f84a9ba33b493dbf6fcdc86bcce5eafbbf74
Zenon Lovo <lovo.chavarria@gmail.com>
2025-09-06 03:37:39 +0000

    Revert "Caso de prueba 4"
    This reverts commit 87336dd98d6f5d04258cb4e0766e2bf6417cde69.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-const app = require("./logout");
-const PORT = 3000;
+require('dotenv').config();
+const app = require('./app');
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`API escuchando en http://localhost:${PORT}`);
 });
